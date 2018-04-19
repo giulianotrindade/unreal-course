dd29cc2f17486921cca934e64d2e65e8aa584085
giuliano <giuliano.trindade.s@gmail.com>
2018-04-19 18:17:01 -0300

    Aula 1

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Giuliano Trindade Sosnonski on 19/04/2018.
+//  Copyright © 2018 Giuliano Trindade Sosnonski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello world\n";
+    return 0;
+}
